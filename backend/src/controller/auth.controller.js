0050e75fed82cd31c260b945bf7b325a3d41d788
Khushal <Khushal.badaya@gmail.com>
2026-03-22 17:49:13 +0530

    login and logout created

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,40 +1,70 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-export const signup = async(req,res) =>{
-    const {fullName,email,password} = req.body;
-    try {
-        if(!fullName || !email || !password ){
-            return res.status(400).json({message : "All feilds required"});
-        }
-        if(password.length()<6){
-            return res.status(400).json({message:"Password should be 6 characters or more"});
-        }
-        const user = await User.findOne({email});
-        if(user){
-            return res.status(400).json({message:"Email Already Existed"});
-        }
-        
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password,salt);
-
-        const newUser = new User({
-            fullName:fullName,
-            email:email,
-            password:hashedPassword
-        })
-
-        const savedUser = await  newUser.save()
-        generateToken(savedUser._id, res);
-
-        res.status(201).json({
-            _id:savedUser._id,
-            fullName:savedUser.fullName,
-            email:savedUser.email,
-            profilePic:savedUser.profilePic
-        })
-    } catch (error) {
-        console.log("Signup Error",error);
-        res.status(500).json({message:"Internal Server Error"});
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All feilds required" });
     }
-}   
+    if (password.length() < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password should be 6 characters or more" });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email Already Existed" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName: fullName,
+      email: email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    generateToken(savedUser._id, res);
+
+    res.status(201).json({
+      _id: savedUser._id,
+      fullName: savedUser.fullName,
+      email: savedUser.email,
+      profilePic: savedUser.profilePic,
+    });
+  } catch (error) {
+    console.log("Signup Error", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if ((!email, !password))
+      return res.status(400).jsonn({ message: "All feilds Required" });
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).jsonn({ message: "User not exist!" });
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect)
+      return res.status(400).jsonn({ message: "Invaild Credentials" });
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Login Error", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const logout = async (_, res) => {
+  res.cookie("jwt", "", { maxAge: 0, sameSite: "none", secure: true });
+  res.status(200).json({ message: "Logout Successfully" });
+};
