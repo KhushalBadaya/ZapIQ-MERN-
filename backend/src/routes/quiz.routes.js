42a9f4673f4757b9ba3483eb7e2a88c58c52591a
Khushal <Khushal.badaya@gmail.com>
2026-03-25 01:03:00 +0530

    fixed soe bugs

--- backend/src/routes/quiz.routes.js
+++ backend/src/routes/quiz.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { protectedRoute } from "../middleware/auth.middleware";
-import { result, submit } from "../controller/quiz.controller";
+import { protectedRoute } from "../middleware/auth.middleware.js";
+import { getQuiz, result, submit } from "../controller/quiz.controller.js";
 
 const router = express.Router()
-router.get("/api/quiz/:id");
+router.get("/api/quiz/:id",getQuiz);
 router.post("/api/quiz/:id/submit",protectedRoute,submit);
 router.post("/api/quiz/:id/result",protectedRoute,result);
+
+export default router;
