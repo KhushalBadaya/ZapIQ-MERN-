cd70da0844c15178f1bd529d938aac8c4dd2effe
Khushal <Khushal.badaya@gmail.com>
2026-04-02 01:51:24 +0530

    basic ui of dashboard created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import HomePage from "./pages/HomePage.jsx";
 import SignupPage from "./pages/SignupPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 function App() {
   const {authUser,checkAuth,isCheckingAuth} = useAuthStore();
   useEffect(() => {
@@ -18,6 +19,7 @@ return (
      <Route path="/" element={<HomePage />} />
     <Route path="/signup" element={<SignupPage />} />
     <Route path="/login" element={<LoginPage/>} />
+    <Route path="/dashboard" element={<Dashboard/>} />
     </Routes>
   );
 }
